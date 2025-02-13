9ef332eb78b5827de7c586d11d3a38da956dc3db
Dev86-git <devaraj.kushi@gmail.com>
2025-02-13 03:32:39 +0000

    Revert "adding the disk file"
    This reverts commit fde8ca43a48ba748a33f6c66e6166bf6be6b2948.

--- disk.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-size=`df -h . | tail -1 | awk -F " " '{print$(NF-1)}' | sed 's/%//g'`
-if [ $size -gt 30 ]; then
-body=`echo -e "memory reached 30%, please take appropriate action"`
-echo $body | mail -s "memory reached 30%" "devaraj.kushi@gmail.com"
-fi
